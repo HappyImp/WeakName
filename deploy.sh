33beab11615809647c69ecc9f52238ba2dbe5069
kyluzoi <kyluzoi@gmail.com>
2021-06-07 15:21:51 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,19 @@
-!/usr/bin/env sh# 确保脚本抛出遇到的错误set -e
-# 生成静态文件npm run docs:build
-# 进入生成的文件夹cd docs/.vuepress/dist
+#!/usr/bin/env sh
 
+# 确保脚本抛出遇到的错误
+set -e
 
-git initgit add -Agit commit -m 'deploy'
+# 生成静态文件
+npm run docs:build
 
-git push -f git@github.com:HappyImp/WeakName.io.git master
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
+git push -f https://github.com/HappyImp/kyluzoi.io.git master
+
+cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     description: '欢迎来到我的 Blog',
     // 注入到当前页面的 HTML <head> 中的标签
     head: [
-        ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+        ['link', {rel: 'icon', href: '/favicon.ico'}], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     // base: '/web_accumulate/', // 这是部署到github相关的配置 下面会讲
     markdown: {
@@ -12,5 +12,6 @@ module.exports = {
     themeConfig: {
         sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
         lastUpdated: 'Last Updated' // 文档更新时间：每个文件git最后提交的时间
-    }
+    },
+    base: '/WeakName.io/'
 };
